5ffc8392e8f34d87bcde9e71ea2236cf690501d8
fikretellek <fikretellek@icloud.com>
2024-04-11 04:09:21 +0100

    logo updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ data }) {
   return (
     <header>
       <div id="logo-container">
-        <img className="logo" src="src/assets/logo-white.png" alt="" />
+        <img className="logo" src={data.logo.light} alt="" />
       </div>
 
       <h3 id="name-logo">{data.personalInfo.siteName}</h3>
